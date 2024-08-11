6a3340e3b309a7e45cb1834e630a55458ee3bb44
Marcus441 <psminchia4153@hotmail.com>
2024-08-11 16:30:34 +1000

    added java support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
   { import = "plugins.spec.autosession" },
   { import = "plugins.spec.harpoon" },
   { import = "plugins.spec.vim-tmux-navigator" },
+  { import = "plugins.spec.nvim-java" },
   { import = "plugins.override.mason" },
   { import = "plugins.override.nvim-treesitter" },
   { import = "plugins.override.lspconfig" },
